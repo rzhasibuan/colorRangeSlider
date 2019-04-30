c39593af0f059bbc0ec25c9d4e21f1c4d2b1521e
rzhasibuan <49144713+rzhasibuan@users.noreply.github.com>
2019-05-01 01:35:42 +0700

    update

--- js/rangeColor.js
+++ js/rangeColor.js
@@ -1,17 +1,23 @@
-const sMerah = document.querySelector('input[name=sMerah]');
-const sHijau = document.querySelector('input[name=sHijau]');
-const sBiru = document.querySelector('input[name=sBiru]');
+// write by reza afri suhangga hasibuan
+// a.k.a rzhasibuan
+const sRed = document.querySelector('input[name=sRed]');
+const sGreen = document.querySelector('input[name=sGreen]');
+const sBlue = document.querySelector('input[name=sBlue]');
+const valueRGB = document.querySelector('#value');
 
+valueRGB.innerHTML = "(" + sRed.value + "," + sGreen.value + "," + sBlue.value + ")";
+document.body.style.backgroundColor = 'rgb(' + sRed.value + ',' + sGreen.value + ',' + sBlue.value + ')';
 
 function color(tombol, sr, sg, sb) {
     tombol.addEventListener('input', function () {
         const r = sr.value;
         const g = sg.value;
-        const b = sb.value
+        const b = sb.value;
         document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+        valueRGB.innerHTML = "(" + r + "," + g + "," + b + ")";
     });
 }
 
-color(sMerah, sMerah, sHijau, sBiru);
-color(sHijau, sMerah, sHijau, sBiru);
-color(sBiru, sMerah, sHijau, sBiru);
+color(sRed, sRed, sGreen, sBlue);
+color(sGreen, sRed, sGreen, sBlue);
+color(sBlue, sRed, sGreen, sBlue);
